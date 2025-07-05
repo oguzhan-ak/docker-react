22013275d69cf2637c3cd63abdc259bb23ba3788
oguzhan-ak <oguzhan_ak@outlook.com.tr>
2025-07-05 16:46:32 +0300

    .

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Byee there!
+          Byee there!ddfg
         </a>
       </header>
     </div>
